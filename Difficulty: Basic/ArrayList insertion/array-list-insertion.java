8eed8d6ac1026a4fc62fb203b0fb9befff7a51d2
Mohd-Zeeshan-khan <mzkhan82004@gmail.com>
2026-07-06 22:16:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/ArrayList insertion/array-list-insertion.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static ArrayList<Integer> fillArrayList(int arr[]) {
+        // Your code here
+         ArrayList<Integer> addition = new ArrayList<>();
+         for( int i : arr){
+             addition.add(i);
+             
+         }
+         return addition;
+    }
+}
