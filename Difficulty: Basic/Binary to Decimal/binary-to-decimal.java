87df97e31b0c57f8f76e44a502f7febb55fd45fd
Mohd-Zeeshan-khan <mzkhan82004@gmail.com>
2026-08-09 19:23:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary to Decimal/binary-to-decimal.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int binaryToDecimal(String b) {
+        // code here
+        int decimalValue = Integer.parseInt(b, 2);        
+        return decimalValue;
+    }
+}
